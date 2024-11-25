25c90a9073e386a2487fd32185cc7d2adf41ade1
madscientist-314 <78171604+madscientist-314@users.noreply.github.com>
2024-11-25 16:16:38 +0000

    0.2.7 Admin Analysis  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   admin.html
    	modified:   admin/manage.html
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   server.js
    	modified:   static/style.css

--- server.js
+++ server.js
@@ -25,6 +25,61 @@ function getKitDataByID(records) {
   console.log(records.recordset.length);
 }
 
+function writeKitData(request, writeQuery) {
+  var query = `
+    INSERT INTO kit_data(
+      contents_of_qr_code, status, category, manufacturer, attribute_brand, 
+      attribute_color, attribute_size, is_a_set, number_in_set, 
+      number_present_in_set, purchased_date, purchased_value, purchased_from, 
+      next_inspection_date, last_inspection_date, working_life_in_years, 
+      retirement_date, on_loan, notes
+    ) VALUES `;
+  
+    for (let i = 0; i < writeQuery.length; i++) {
+      query += `('${writeQuery[i].join("', '")}')`;
+      if (i < writeQuery.length - 1) {
+        query += ', ';
+      }
+    }
+  
+    query += ';';
+    
+  request.batch(query, (err, result) => {
+    if (err) {
+      console.error('Error:', err);
+    } else {
+      console.log('Result:', result);
+    }
+  });
+}
+    
+function checkLoginData(request, username, password) {
+  request.query("SELECT * FROM login", function (err, recordset) {
+    if (err) {
+      console.error('Query error:', err);
+      return res.status(500).send('Query error');
+    }      
+
+    const loginData = recordset.recordset;
+    for (let i = 0; i < loginData.length; i++) {
+      if (loginData[i].username === username && loginData[i].password_hash === password) {
+        console.log("Login successful");
+        console.log("User ID:", loginData[i].uid);
+        request.query("SELECT * FROM kit_hire WHERE user_id = " + loginData[i].uid, function (err, recordset) {
+          if (err) {
+            console.error('Query error:', err);
+            return res.status(500).send('Query error');
+          }
+          //data = recordset.recordset[0].loan_start_date;
+          console.log("Kit hire data:", recordset.recordset[0]);
+        });
+        return "Login successful";
+      }
+    }
+    console.log("Login failed");
+  });
+}
+
 const formData = [
   ["SCD-ST-L35", "Missing", "Stoves", "Trangia", "Trangia 25-1/UL", "Silver", "L", 0, 0, 0, '2018-08-01', '121.56', "Trangia", '2024-01-10', '2021-01-10', 10, '2000-01-01', 0, ""],
   ["SCD-ST-L36", "Available", "Stoves", "Trangia", "Trangia 25-2/UL", "Silver", "M", 0, 0, 0, '2019-08-01', '130.00', "Trangia", '2025-01-10', '2022-01-10', 10, '2001-01-01', 0, ""]
@@ -52,17 +107,178 @@ const config = {
     encrypt: false,
     enableArithAbort: true,
     trustServerCertificate: true,
-  }
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
 };
-
-mssql.connect(config); // Connect to the database
-// otside of the app.get("/", (req, res) => { ... }) function to avoid multiple connections (CONNRESET error)
 
 // Middleware to parse JSON and URL-encoded data
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Create a connection pool
+let poolPromise;
+async function getConnection() {
+  if (!poolPromise) {
+    poolPromise = mssql.connect(config); // Connect to the database
+  }
+  return poolPromise;
+} // outside of the app.get("/", (req, res) => { ... }) function to avoid multiple connections (CONNRESET error)
+
+// Handle form submissions
+app.post("/submit-form", (req, res) => {
+  console.log("Form submission received"); // Log when the form is received
+
+  const formData = req.body;
+  console.log("Form data:", formData); // Log the form data
+
+  emailMessage = formData["userName"] + " has requested to hire kit for " + formData["event"] + " from " + formData["startDate"] + " to " + formData["endDate"] + ".\nThey have provided the following details:\n" + formData["userMessage"] + ".\nYou can contact them at " + formData["userEmail"] + " to confirm the hire.\nTheir school/company is " + formData["company"] + ".\nPostcode: " + formData["userPostCode"] + ".\nPhone: " + formData["userPhone"];
+  console.log("Email message:", emailMessage);
+
+  // Defining the email options
+  const mailOptions = {
+    from: "noreply.kitlogger@gmail.com",
+    to: "dofe@scd.herts.sch.uk",
+    subject: "Form Submission",
+    text: emailMessage,
+  };
+
+  // Creating a transporter object using SMTP transport
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'noreply.kitlogger@gmail.com', // Sender's email
+      pass: 'krwg eyqp qlhq ioln', // Email account app password
+    },
+  });
+
+  // Sending the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      return res.status(500).send(error.toString());
+    }
+    console.log('Email sent:', info.response);
+    return res.status(200).send("Email sent: " + info.response);
+  });
+});
+
+// Handle fetching data from the database
+app.get("/fetch-data", async (req, res) => {
+  try {
+    const pool = await getConnection();
+    console.log('Database connection established');
+
+    const request = pool.request();
+    const result = await request.query("SELECT * FROM kit_data ORDER BY contents_of_qr_code ASC");
+
+    res.send(result.recordset); // Send the recordset to the client
+    console.log(result.recordset.length); // Log the number of records
+  } catch (err) {
+    console.error('Query error:', err);
+    res.status(500).send('Query error');
+  }
+});
+
+// Handle Search
+app.get("/search-data", async (req, res) => {
+  const searchRequest = req.query.search; // Get the search query parameter
+  const category = req.query.category;
+
+  console.log('Search request:', searchRequest);
+  console.log('Category:', category);
+
+  if (!searchRequest || !category) {
+    return res.status(400).send('Search query and category are required');
+  }
+
+  try {
+    const pool = await getConnection();
+    console.log('Database connection established');
+
+    const request = pool.request();
+    request.input('searchRequest', mssql.VarChar, `%${searchRequest}%`);
+    const query = `SELECT * FROM kit_data WHERE ${category} LIKE @searchRequest`;
+    const result = await request.query(query);
+
+    res.send(result.recordset);
+  } catch (err) {
+    console.error('DB Query error:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get("/kit-analysis", async (req, res) => {
+  try {
+    const pool = await getConnection();
+    console.log('Database connection established');
+
+    const request = pool.request();
+    const allItems = await request.query("SELECT * FROM kit_data");
+    const onLoanItems = await request.query("SELECT * FROM kit_data WHERE on_loan = '1'");
+    const availableItems = await request.query("SELECT * FROM kit_data WHERE status = 'available'");
+    const missingItems = await request.query("SELECT * FROM kit_data WHERE status = 'missing'");
+
+    const result = await Promise.all([allItems.recordset.length, onLoanItems.recordset.length, availableItems.recordset.length, missingItems.recordset.length]);
+    console.log(result);
+
+    res.json(result);
+  } catch (err) {
+    console.error('Query error:', err);
+    res.status(500).send('Query error');
+  }
+});
+
+app.get("/kit-analysis-by-category", async (req, res) => {
+  const category = req.query.category;
+
+  if (!category) {
+    return res.status(400).send('Category is required');
+  }
+
+  try {
+    const pool = await getConnection();
+    console.log('Database connection established');
+
+    const request = pool.request();
+    request.input('category', mssql.VarChar, `%${category}%`);
+    const allItems = await request.query("SELECT * FROM kit_data WHERE category LIKE @category");
+    const onLoanItems = await request.query("SELECT * FROM kit_data WHERE category LIKE @category AND on_loan = '1'");
+    const availableItems = await request.query("SELECT * FROM kit_data WHERE category LIKE @category AND status = 'available'");
+    const missingItems = await request.query("SELECT * FROM kit_data WHERE category LIKE @category AND status = 'missing'");
+    
+    const result = await Promise.all([allItems.recordset.length, onLoanItems.recordset.length, availableItems.recordset.length, missingItems.recordset.length]);
+    console.log(result);
+
+    res.json(result);
+  } catch (err) {
+    console.error('Query error:', err);
+    res.status(500).send('Query error');
+  }
+});
+
+// Handle login form submissions
+app.post("/submit-login-form", (req, res) => {
+  console.log("Form submission received"); // Log when the form is received
+
+  const loginFormData = req.body;
+  console.log("Form data:", loginFormData); // Log the form data
+
+  const username = loginFormData["uname"];
+  const password = loginFormData["psw"];
+  console.log("Username:", username);
+  console.log("Password:", password);
+
+  // Add your login logic here
+
+  res.send("Login form submitted");
+});
+
+/*
 // Handle form submissions
 app.post("/submit-form", (req, res) => {
   console.log("Form submission received"); // Log when the form is received
@@ -119,13 +335,13 @@ app.get("/fetch-data", (req, res) => {
 
 // Handle Search
 app.get("/search-data", (req, res) => {
-  var searchRequest = req.query.search; // Get the search query parameter
+  const searchRequest = req.query.search; // Get the search query parameter
   const category = req.query.category;
 
   console.log('Search request:', searchRequest);
   console.log('Category:', category);
 
-  if (!searchRequest) {
+  if (!searchRequest || !category) {
     return res.status(400).send('Search query is required');
   }
   
@@ -140,100 +356,32 @@ app.get("/search-data", (req, res) => {
     res.send(result.recordset);
   });
 });
+*/
+app.get("/kit-analysis", (req, res) => {
+
+  const request = new mssql.Request();
+  const onLoanItems = request.query("SELECT * FROM kit_data WHERE on_loan = '1'");
+  const availableItems = request.query("SELECT * FROM kit_data WHERE status = 'available'");
+  const missingItems = request.query("SELECT * FROM kit_data WHERE status = 'missing'");
+  
+  const result = Promise.all([onLoanItems, availableItems, missingItems]);
+    console.log(result);
+  
+  res.send(result);
+
+});
+
 
 /*
-// Handle database connections
-app.get("/", (req, res) => {
-  console.log("Database connection details: ", config);
-
-  mssql.connect(config, function (err) {
-    if (err) {
-      console.error('Database connection error:', err);
-      return res.status(500).send('Database connection error');
-    }
-    
-    const request = new mssql.Request()
-
     //request.query("CREATE TABLE login(user_id INT IDENTITY(1,1) PRIMARY KEY,	username VARCHAR(50), password_hash VARCHAR(50), encrypt_key VARCHAR(50), exped_level VARCHAR(6), loan_paid BIT, no_of_loaned_items INT);", function (err, success) {});
     //request.query("CREATE TABLE kit_hire(user_id INT PRIMARY KEY, loan_start_date DATE, loan_end_date DATE, loan_name VARCHAR(50), loan_email VARCHAR(70), loan_paid BIT, rental_value VARCHAR(10), kit_id1 INT, kit_id2 INT, kit_id3 INT, kit_id4 INT, kit_id5 INT);", function (err, success) {});
 */
-    function writeKitData(request, writeQuery) {
-      var query = `
-        INSERT INTO kit_data(
-          contents_of_qr_code, status, category, manufacturer, attribute_brand, 
-          attribute_color, attribute_size, is_a_set, number_in_set, 
-          number_present_in_set, purchased_date, purchased_value, purchased_from, 
-          next_inspection_date, last_inspection_date, working_life_in_years, 
-          retirement_date, on_loan, notes
-        ) VALUES `;
-      
-        for (let i = 0; i < writeQuery.length; i++) {
-          query += `('${writeQuery[i].join("', '")}')`;
-          if (i < writeQuery.length - 1) {
-            query += ', ';
-          }
-        }
-      
-        query += ';';
-        
-      request.batch(query, (err, result) => {
-        if (err) {
-          console.error('Error:', err);
-        } else {
-          console.log('Result:', result);
-        }
-      });
-    }
     
-
-
-/*
-    function getKitData(request) {
-      request.query("SELECT * FROM kit_data", function (err, recordset) {
-        if (err) {
-          console.error('Query error:', err);
-          return res.status(500).send('Query error');
-        }
-
-        res.send(recordset.recordset);
-        getKitDataByID(recordset);
-        
-      });
-    }
-
-    function checkLoginData(request, username, password) {
-      request.query("SELECT * FROM login", function (err, recordset) {
-        if (err) {
-          console.error('Query error:', err);
-          return res.status(500).send('Query error');
-        }      
-
-        const loginData = recordset.recordset;
-        for (let i = 0; i < loginData.length; i++) {
-          if (loginData[i].username === username && loginData[i].password_hash === password) {
-            console.log("Login successful");
-            console.log("User ID:", loginData[i].uid);
-            request.query("SELECT * FROM kit_hire WHERE user_id = " + loginData[i].uid, function (err, recordset) {
-              if (err) {
-                console.error('Query error:', err);
-                return res.status(500).send('Query error');
-              }
-              //data = recordset.recordset[0].loan_start_date;
-              console.log("Kit hire data:", recordset.recordset[0]);
-            });
-            return "Login successful";
-          }
-        }
-        console.log("Login failed");
-      });
-    }
-    
+  /*  
     const username = "rfaughny2";
     const password = "$2a$04$hYFCtwlw8MLiSqMl.JaMSeZbCgI6jZqPPSzb2wDHeU8Xo/GLz.z0u";
-    console.log(checkLoginData(request, username, password));
-  });
-}); */
-
+    console.log(checkLoginData(request, username, password));*/
+/*
 // Handle login form submissions
 app.post("/submit-login-form", (req, res) => {
   console.log("Form submission received"); // Log when the form is received
@@ -252,6 +400,9 @@ app.post("/submit-login-form", (req, res) => {
     console.log("Remember me: false");
   }
 });
+
+
+*/
 
 // Start the server
 app.listen(port, () => {
